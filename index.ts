3f69e74abad680d7512613961224f38523af773b
Rafał Golcz <rgl@touk.pl>
2022-04-20 16:01:10 +0200

    Tryb pracy: ciągły, trigger

--- index.ts
+++ index.ts
@@ -29,7 +29,21 @@ let prev = { x: 0, y: 0 }
 
 let started = false
 let first = true
+let mode = 'raw'
+let triggered = false
 document.onkeydown = (event) => {
+    if (event.code == 'KeyM') {
+        if (mode == 'trigger') {
+            mode = 'raw'
+        } else {
+            mode = 'trigger'
+        }
+    }
+    if (event.code == 'KeyC') {
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+        ctx.fillStyle = '#000f'
+        ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+    }
     if (!started) {
         const context = new AudioContext();
 
@@ -41,32 +55,56 @@ document.onkeydown = (event) => {
             processor.connect(context.destination);
 
             processor.onaudioprocess = function (e) {
-                ctx.fillStyle = '#0001'
-                ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+
 
                 let array = e.inputBuffer.getChannelData(0)
 
-                ctx.lineWidth = 1
-                ctx.fillStyle = 'white'
-                ctx.strokeStyle = 'white'
-                ctx.beginPath()
-                ctx.moveTo(prev.x, prev.y)
-                let y
-                for (let sample of array) {
-                    y = sample * canvas.height / 2 + canvas.height / 2
-                    ctx.lineTo(x, y)
-                    x++
-                    if (x > ctx.canvas.width) {
-                        ctx.stroke()
-                        ctx.beginPath
-                        ctx.moveTo(0, y)
-                        x = 0
+                if (mode == 'raw') {
+                    ctx.fillStyle = '#0001'
+                    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+                    ctx.lineWidth = 1
+                    ctx.fillStyle = 'white'
+                    ctx.strokeStyle = 'white'
+                    ctx.beginPath()
+                    ctx.moveTo(prev.x, prev.y)
+                    let y
+                    for (let sample of array) {
+                        y = sample * canvas.height / 2 + canvas.height / 2
+                        ctx.lineTo(x, y)
+                        x++
+                        if (x > ctx.canvas.width) {
+                            ctx.stroke()
+                            ctx.beginPath
+                            ctx.moveTo(0, y)
+                            x = 0
+                        }
                     }
-                }
-                prev.x = x
-                prev.y = y
-                ctx.stroke()
+                    prev.x = x
+                    prev.y = y
+                    ctx.stroke()
+                } else if (mode == 'trigger') {
+                    ctx.beginPath()
 
+                    for (let i = 0; i < array.length; i++) {
+                        if (triggered) {
+                            let y = array[i] * canvas.height / 2 + canvas.height / 2
+                            ctx.lineTo(x, y)
+                            x++
+                            if (x > ctx.canvas.width) {
+                                ctx.stroke()
+                                ctx.moveTo(0, 0)
+                                x = 0
+                                triggered = false
+                            }
+                        } else {
+                            if (Math.abs(array[i]) > 0.1) {
+                                triggered = true
+                                x = 0
+                            }
+                        }
+                    }
+                    ctx.stroke()
+                }
             };
         });
     }
